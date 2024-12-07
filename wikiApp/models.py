a7af4cabb53fc1def2c7032a17a9287d8f671dcb
Nelson Rios <nelson28.adn@gmail.com>
2024-12-06 20:32:25 -0500

    Primer Avance

--- wikiApp/models.py
+++ wikiApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class temaWiki(models.Model):
+    nombre = models.CharField(max_length=128, null=True, blank=True)
+    descripcion = models.CharField(max_length=512, null=True, blank=True)
+# debe ser de campo tipo texto
+
+class articuloWiki(models.Model):
+    titulo = models.CharField(max_length=128, null=True, blank=True)
+    contenido = models.CharField(max_length=1024, null=True, blank=True)
+# ver como se hace los primarykey y de campo texto
--- wikiApp/views.py
+++ wikiApp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def hola(request):
+    return HttpResponse("Bienvenidos a Examen Parcial")
+
+def vistaPrincipal(request):
+    nombre = "Nelson en vista princial"
+    return render(request,'vistaPrincipal.html',{
+        'nombre':nombre
+    })
+
+def crearArtiulo(request):
+    nombre = "Creando articulo"
+    return render(request,'crearArtiulo.html')
+
+def crearTema(request):
+    nombre = "Crear Tema"
+    return render(request,'crearTema.html')
+
+def verArticulo(request):
+    nombre = "ver Articulo"
+    return render(request,'verArticulo.html')
+
+def buscarArticulo(request):
+    nombre = "Buscar Articulo"
+    return render(request,'buscarArticulo.html')
